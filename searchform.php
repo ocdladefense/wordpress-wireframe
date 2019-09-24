bc279182a5ddf03f156d5ee26bb4faac21168d6d
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2019-09-24 15:25:50 -0700

    added inline style for image on the button

--- searchform.php
+++ searchform.php
@@ -17,7 +17,6 @@
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'wireframe' ); ?></span>
 	</label>
 	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'wireframe' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-	<button type="submit" class="search-submit" style="background-image:">
-		<img class="search-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/search-icon.png" />
+	<button type="submit" class="search-submit" style="background-image:url('<?php echo get_template_directory_uri(); ?>/assets/images/search-icon.png');">
 	</button>
 </form>
