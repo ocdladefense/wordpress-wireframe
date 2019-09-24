7a60f27290ede5939cad9ccc1128ae8b310edcfa
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2019-09-24 13:06:40 -0700

    global header styles

--- searchform.php
+++ searchform.php
@@ -17,7 +17,7 @@
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'wireframe' ); ?></span>
 	</label>
 	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'wireframe' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-	<button type="submit" class="search-submit">
+	<button type="submit" class="search-submit" style="background-image:">
 		<img class="search-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/search-icon.png" />
 	</button>
 </form>
