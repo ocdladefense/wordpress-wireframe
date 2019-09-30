34c5509676c04e989be2f8285664fa6f10985191
Jose Bernal <jbernal.web.dev@gmail.com>
2019-09-30 21:49:46 +0000

    More fonts to choose from.

--- header.php
+++ header.php
@@ -15,10 +15,11 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800&display=swap" rel="stylesheet">
-<link rel="profile" href="http://gmpg.org/xfn/11">
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800&display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Prompt&display=swap" rel="stylesheet" />
+<link rel="profile" href="http://gmpg.org/xfn/11" />
 
 <?php wp_head(); ?>
 </head>
