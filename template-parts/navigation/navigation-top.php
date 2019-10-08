c7fc35bbd726a8b2099043cfc84ed6747c2f53ed
Jose Bernal <jbernal.web.dev@gmail.com>
2019-10-08 18:09:08 +0000

    Normalize top menu w/o extra styles.

--- template-parts/navigation/navigation-top.php
+++ template-parts/navigation/navigation-top.php
@@ -10,28 +10,16 @@
 
 ?>
 <!--USE ID & CLASSNAME OF "main-navigation" if you want to have the menu collapsible on smaller screens?-->
-<nav id="main-navigation" class="menu main-navigation top-navigation navigation-collapsible menu-button" role="navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'wireframe' ); ?>">
-	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">
-		<?php
-		echo wireframe_get_svg( array( 'icon' => 'bars' ) );
-		echo wireframe_get_svg( array( 'icon' => 'close' ) );
-		_e( 'Dork', 'wireframe' );
-		?>
-	</button>
+<nav class="menu collapsible menu-button" role="navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'wireframe' ); ?>">
 
-	<?php
-	if(has_nav_menu('top')) {
-		echo "<h3>Hello World!</h3>";
+	<?php if(has_nav_menu('top')):
 		wp_nav_menu(
 			array(
 				'theme_location' => 'top',
 				'menu_id'        => 'top-menu',
+				'menu_class'		 => 'horizontal light-background'
 			)
 		);
-	}
-	?>
+	endif; ?>
 
-	<?php if ( ( wireframe_is_frontpage() || ( is_home() && is_front_page() ) ) && has_custom_header() ) : ?>
-		<a href="#content" class="menu-scroll-down"><?php echo wireframe_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'wireframe' ); ?></span></a>
-	<?php endif; ?>
-</nav><!-- #site-navigation -->
+</nav>
